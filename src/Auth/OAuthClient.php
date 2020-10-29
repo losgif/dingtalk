922a60b056781665a6989c4d2665bd87b7688661
losgif <losgif@gmail.com>
2020-10-29 11:25:36 +0800

    Update OAuthClient.php

--- src/Auth/OAuthClient.php
+++ src/Auth/OAuthClient.php
@@ -120,6 +120,20 @@ class OAuthClient extends Client
 
         return $this->postJson('sns/getuserinfo_bycode', $data, $query);
     }
+    
+    /**
+     * JSON request.
+     *
+     * @param string       $url
+     * @param string|array $data
+     * @param array        $query
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\Overtrue\Http\Support\Collection|array|object|string
+     */
+    public function postJson(string $url, array $data = [], array $query = [])
+    {
+        return $this->request($url, 'POST', ['query' => $query, 'json' => $data]);
+    }
 
     /**
      * 计算签名
